ddb41d3eb5aed315b6fab2fca9d807496d92e174
Pierre Cruz <contact@pierrecruz.com>
2021-07-30 00:53:50 +0200

    #138

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { json } from "body-parser";
 
+// Middlewares
+import { errorHandler } from "./middlewares/error-handlers";
+
+// Routes
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -13,6 +17,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
