1b5756b1d201ec0da3574ecfc5eea9ad12b18115
Pierre Cruz <contact@pierrecruz.com>
2021-08-02 14:23:48 +0200

    #192

---
+++ auth/src/app.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+
+import cookieSession from "cookie-session";
+
+// Middlewares
+import { errorHandler } from "./middlewares/error-handlers";
+
+// Routes
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+// Errors
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+// Traffic pass through proxy ingress-nginx
+app.set("trust proxy", true);
+app.use(json());
+
+// Service use cookie
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+// Router
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+// Error handler
+app.use(errorHandler);
+
+export { app };
