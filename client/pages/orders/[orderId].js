92a56c031c1dcb7f70aebdccf35da24683df1e0c
Pierre Cruz <contact@pierrecruz.com>
2021-08-31 16:45:15 +0200

    #488

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import Router from 'next/router';
-import useRequest from '../../hooks/use-request';
+import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
 
 const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
   const { doRequest, errors } = useRequest({
-    url: '/api/payments',
-    method: 'post',
+    url: "/api/payments",
+    method: "post",
     body: {
       orderId: order.id,
     },
-    onSuccess: () => Router.push('/orders'),
+    onSuccess: () => Router.push("/orders"),
   });
 
   useEffect(() => {
@@ -37,7 +37,7 @@ const OrderShow = ({ order, currentUser }) => {
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
         token={({ id }) => doRequest({ token: id })}
-        stripeKey="pk_test_JMdyKVvf8EGTB0Fl28GsN7YY"
+        stripeKey="pk_test_51JUVXJA6sZRDf3Wn3Hf5GB3hn4LwTdQrpaOkwrujJhlGrt2OpFQdhh2N2Qlh0dYo0zkLrpgJq3guilCjU1zrvpUX00gQnBnJD4"
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
