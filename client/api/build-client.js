a1d85c20a12ffb14b4fa8dfec27ee8445e125bed
Pierre Cruz <contact@pierrecruz.com>
2021-09-01 15:18:31 +0200

    Update domain name

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,10 +3,8 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.background-note.com",
       headers: req.headers,
     });
   } else {
